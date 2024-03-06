8b24fbb89ecf39792a170ed660e0a71ae75775b9
Akshara Panikar <131359971+aksharap2005@users.noreply.github.com>
2024-03-06 20:04:34 +0530

    Add files via upload

---
+++ celsius to fahrenheit.py
@@ -0,0 +1,7 @@
+#wap to convert celsius into fahrenheit  ((1.8*celsius)+32)
+
+celsius=float(input("Enter celsius"))
+def temp():
+    fahrenheit=(1.8*celsius)+32
+    print("Celsius to fahrenheit is:",fahrenheit)
+temp()
